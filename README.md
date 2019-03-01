0e92f1eb2ba943ab669f8f53066b864dfd96d80a
James Langley <jwl4017@point46.doc.ic.ac.uk>
2019-03-01 15:31:20 +0000

    Update install script to better include external plugins

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-TODO
+# Set up my preferred terminal environment for linux (Ubuntu).
+
+## Installation
+Run the following command:
+    sh -c "$(wget https://raw.githubusercontent.com/jwblangley/zsh-setup/master/setup.sh -O -)"
--- setup.sh
+++ setup.sh
@@ -10,5 +10,16 @@ which micro || snap install micro --classic
 # Download and run oh-my-zsh script.
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# Clone zsh-autosuggestions external plugin.
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# Apply initial changes.
+source "$HOME/.zshrc"
+
+# Clone zsh-autosuggestions external plugin, if not already installed
+ls /homes/jwl4017/.oh-my-zsh/custom/plugins/zsh-autosuggestions || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Clone zsh-syntx-highlighting external plugin, if not already installed
+ls /homes/jwl4017/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Overwrite .zshrc file with my preferred changes.
+wget https://raw.githubusercontent.com/jwblangley/zsh-setup/master/.zshrc -O "$HOME/.zshrc"
+
+# Apply changes.
+source "$HOME/.zshrc"
